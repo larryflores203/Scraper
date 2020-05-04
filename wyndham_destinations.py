996ba858a2f1ac3db15939450e34567b191f7e3a
larryflores203 <64392613+larryflores203@users.noreply.github.com>
2020-05-03 21:02:40 -0400

    Second name for rooms attempt
    Attempted to make if statement to print both names of the room if the html path existed to the second name and if not than to just print the first name and move on.

--- wyndham_destinations.py
+++ wyndham_destinations.py
@@ -6,7 +6,7 @@ import re
 
 class wynSpider(scrapy.Spider):
     name = "wynSpider"
-    start_urls = ['https://clubwyndham.wyndhamdestinations.com/us/en/resorts/wyndham-hotels-resorts/united-states-of-america/florida/orlando/worldmark-orlando-kingstown-reef']
+    start_urls = ['https://clubwyndham.wyndhamdestinations.com/us/en/resorts/wyndham-hotels-resorts/united-states-of-america/florida/orlando/club-wyndham-bonnet-creek']
 
     def parse(self, response):
         # This for loop gets the resort data; name, address, telephone, quote, amenities,
@@ -33,11 +33,36 @@ class wynSpider(scrapy.Spider):
         unit = '//*[@* = "wyn-bedroom-card"]'
         for bedroom in response.xpath(unit):
 
-            room_typeSELECTOR = './/div[@class = "wyn-type-title-2 wyn-color-black"]/text()'
+            room_type_1SELECTOR = './/div[@class = "wyn-type-title-2 wyn-color-black"]/text()'
+            # for presidential, presidential reserve, and other 'unique' units with second name
+            # room_type_2SELECTOR = './/d[@class ="wyn-bedroom-card__detail__row"]/div[@class = "wyn-type-title-2 wyn-color-black"][2]/text()'
             sqft_guestCountSELECTOR = './/div[@class = "wyn-color-grey wyn-type-body-2 wyn-color-grey"]/text()'
             bed_typeSELECTOR = 'normalize-space(.//div[@class = "wyn-bedroom-card__detail__row"]/div[@class = "wyn-color-grey wyn-type-body-2 wyn-color-grey wyn-bedroom-card__detail__row__icons"]/text())'
             num_bathsSELECTOR = './/div[@class = "wyn-bedroom-card__detail__row"]/div[@class = "wyn-color-grey wyn-type-body-2 wyn-color-grey"]/text()'
             room_amenitiesSELECTOR = './/li[@class = "wyn-color-grey wyn-type-body-2"]/text()'
+            # this is for the second room name
+            #room_exists = bedroom.xpath(room_type_2SELECTOR)
+
+            ############################################################################
+            # Attempt to figure out how to print both names for room if it exists and print out the one name
+            # if it does not have a second name
+            # second name for rooms exist in a separate div as a sibling
+            # to the first div with the room's first name
+            # room = []
+            # for room in room_exists:
+            #     if room_exists is not None:
+            #         room_name_1 = bedroom.xpath(
+            #             room_type_1SELECTOR).get().strip()
+            #         room_name_2 = bedroom.xpath(
+            #             room_type_2SELECTOR).get().strip()
+            #         room_name = room_name_1 + " " + room_name_2
+            #         room.append(str(room_name))
+            #     else:
+            #         room_name = bedroom.xpath(
+            #             room_type_1SELECTOR).get().strip()
+            #         room.append(str(room_name))
+
+            ##############################################################################
 
             # strips the room amenities list of all extra spaces and \n
             ls = bedroom.xpath(room_amenitiesSELECTOR).getall()
@@ -68,7 +93,8 @@ class wynSpider(scrapy.Spider):
                 x for x in room_amenities if not x.startswith('Washer')]
 
             yield {
-                'room_type': bedroom.xpath(room_typeSELECTOR).get().strip(),
+                'room_type': bedroom.xpath(room_type_1SELECTOR).get().strip(),
+                'room_label': bedroom.xpath(room_type_2SELECTOR).get().strip(),
                 'bed_type': bedroom.xpath(bed_typeSELECTOR).get().strip(),
                 # [-1] pulls the last number from the array containing square feet and max_guests
                 # in which the last number [-1] is the max_guest number
